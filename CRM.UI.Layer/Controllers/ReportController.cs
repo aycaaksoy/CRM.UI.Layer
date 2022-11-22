23e22537ed90dc618aa7f8913440e25c27aab424
aycaaksoy <aycaaksoy2000@gmail.com>
2022-11-22 23:27:14 +0300

    pdfs

--- CRM.UI.Layer/Controllers/ReportController.cs
+++ CRM.UI.Layer/Controllers/ReportController.cs
@@ -1,9 +1,12 @@
-﻿using CRM.DataAccess.Layer.Concrete;
+﻿using ClosedXML.Excel;
+using CRM.DataAccess.Layer.Concrete;
 using CRM.UI.Layer.Models;
 using iTextSharp.text;
+using iTextSharp.text.pdf;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace CRM.UI.Layer.Controllers
@@ -53,7 +56,41 @@ namespace CRM.UI.Layer.Controllers
         }
         public IActionResult DynamicExcel()
         {
-            return View();
+            using (var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Customer List");
+                workSheet.Cell(1, 1).Value = "Email ";
+                workSheet.Cell(1, 2).Value = "Name ";
+                workSheet.Cell(1, 3).Value = "Surname ";
+
+                int rowCount = 2;
+                foreach(var item in CustomerList()) 
+                {
+                    workSheet.Cell(rowCount, 1).Value = item.Email;
+                    workSheet.Cell(rowCount, 2).Value = item.Name;
+                    workSheet.Cell(rowCount, 3).Value = item.Surname;
+                    rowCount++;
+                }
+                using(var stream=new MemoryStream())
+                {
+                    workBook.SaveAs(stream);    
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","customers_list.xlsx");
+                }
+            }
+        }
+
+        public IActionResult StaticPdfReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/PdfReports/"+"Customer_list.pdf");
+            var stream=new FileStream(path,FileMode.Create);
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+            document.Open();    
+            Paragraph paragraph = new Paragraph("Static writing...");
+            document.Add(paragraph);    
+            document.Close();
+            return File("/PdfReports/Customer_list.pdf","application/pdf","Customer_list.pdf");
         }
     }
 }
